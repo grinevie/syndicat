89e2ad0574b774b45835a02e232558ca1f40ebed
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2021-10-05 09:38:32 +0300

    Update files

--- js/script.js
+++ js/script.js
@@ -1,107 +1,128 @@
-$("a.scroll-to").click(function() {
-    $("html, body").animate({
-       scrollTop: $($(this).attr("href")).offset().top + "px"
-    }, {
-       duration: 500,
-       easing: "swing"
-    });
-    return false;
-});
+// $("a.scroll-to").click(function() {
+//     $("html, body").animate({
+//        scrollTop: $($(this).attr("href")).offset().top + "px"
+//     }, {
+//        duration: 500,
+//        easing: "swing"
+//     });
+//     return false;
+// });
 
-$( ".promocode .button-3" ).click(function() {
-  $( ".footer__form-block" ).addClass("d-block");
-  $( ".footer__content" ).addClass("d-none");
-});
+// $( ".promocode .button-3" ).click(function() {
+//   $( ".footer__form-block" ).addClass("d-block");
+//   $( ".footer__content" ).addClass("d-none");
+// });
 
-//Custom select checkbox
+// //Custom select checkbox
 
 
-function checkboxDropdown(el) {
-    var $el = $(el)
+// function checkboxDropdown(el) {
+//     var $el = $(el)
   
-    function updateStatus(label, result) {
-      if(!result.length) {
-        label.find('.dropdown-label');
-      }
-    };
+//     function updateStatus(label, result) {
+//       if(!result.length) {
+//         label.find('.dropdown-label');
+//       }
+//     };
     
-    $el.each(function(i, element) {
-      var $list = $(this).find('.dropdown-list'),
-        $label = $(this).find('.dropdown-label'),
-        $checkAll = $(this).find('.check-all'),
-        $inputs = $(this).find('.check'),
-        defaultChecked = $(this).find('input[type=checkbox]:checked'),
-        result = [];
+//     $el.each(function(i, element) {
+//       var $list = $(this).find('.dropdown-list'),
+//         $label = $(this).find('.dropdown-label'),
+//         $checkAll = $(this).find('.check-all'),
+//         $inputs = $(this).find('.check'),
+//         defaultChecked = $(this).find('input[type=checkbox]:checked'),
+//         result = [];
       
-      updateStatus($label, result);
-      if(defaultChecked.length) {
-        defaultChecked.each(function () {
-          result.push($(this).next().text());
-          $label.html(result.join(", "));
-        });
-      }
+//       updateStatus($label, result);
+//       if(defaultChecked.length) {
+//         defaultChecked.each(function () {
+//           result.push($(this).next().text());
+//           $label.html(result.join(", "));
+//         });
+//       }
       
-      $label.on('click', ()=> {
-        $(this).toggleClass('open');
-      });
+//       $label.on('click', ()=> {
+//         $(this).toggleClass('open');
+//       });
   
-      $checkAll.on('change', function() {
-        var checked = $(this).is(':checked');
-        var checkedText = $(this).next().text();
-        result = [];
-        if(checked) {
-          result.push(checkedText);
-          $label.html(result);
-          $inputs.prop('checked', false);
-        }else{
-          $label.html(result);
-        }
-          updateStatus($label, result);
-      });
+//       $checkAll.on('change', function() {
+//         var checked = $(this).is(':checked');
+//         var checkedText = $(this).next().text();
+//         result = [];
+//         if(checked) {
+//           result.push(checkedText);
+//           $label.html(result);
+//           $inputs.prop('checked', false);
+//         }else{
+//           $label.html(result);
+//         }
+//           updateStatus($label, result);
+//       });
   
-      $inputs.on('change', function() {
-        var checked = $(this).is(':checked');
-        var checkedText = $(this).next().text();
-        if($checkAll.is(':checked')) {
-          result = [];
-        }
-        if(checked) {
-          result.push(checkedText);
-          $label.html(result.join(", "));
-          $checkAll.prop('checked', false);
-        }else{
-          let index = result.indexOf(checkedText);
-          if (index >= 0) {
-            result.splice(index, 1);
-          }
-          $label.html(result.join(", "));
-        }
-        updateStatus($label, result);
-      });
+//       $inputs.on('change', function() {
+//         var checked = $(this).is(':checked');
+//         var checkedText = $(this).next().text();
+//         if($checkAll.is(':checked')) {
+//           result = [];
+//         }
+//         if(checked) {
+//           result.push(checkedText);
+//           $label.html(result.join(", "));
+//           $checkAll.prop('checked', false);
+//         }else{
+//           let index = result.indexOf(checkedText);
+//           if (index >= 0) {
+//             result.splice(index, 1);
+//           }
+//           $label.html(result.join(", "));
+//         }
+//         updateStatus($label, result);
+//       });
   
-      $(document).on('click touchstart', e => {
-        if(!$(e.target).closest($(this)).length) {
-          $(this).removeClass('open');
-        }
-      });
-    });
-  };
+//       $(document).on('click touchstart', e => {
+//         if(!$(e.target).closest($(this)).length) {
+//           $(this).removeClass('open');
+//         }
+//       });
+//     });
+//   };
   
-  checkboxDropdown('.dropdown'); 
+//   checkboxDropdown('.dropdown'); 
 
 //Form Sheet 
 
-var $form = $('form#form-sheet'),
-    url = 'https://script.google.com/macros/s/AKfycbz-uAIvKFuT-SWsJJi4TXh0FIj9QF-VfzqFA_B5USY33JvjSDGA/exec'
+// var $form = $('form#form-sheet'),
+//     url = 'https://script.google.com/macros/s/AKfycbz-uAIvKFuT-SWsJJi4TXh0FIj9QF-VfzqFA_B5USY33JvjSDGA/exec'
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
-})
+// $('#submit-form').on('click', function(e) {
+//   e.preventDefault();
+//   var jqxhr = $.ajax({
+//     url: url,
+//     method: "GET",
+//     dataType: "json",
+//     data: $form.serializeObject()
+//   }).success(
+//     // do something
+//   );
+// })
+
+const id = 'AKfycbyGv3hbCu-rHkQibDU_06vDw8kuQMVS0PKqM-hiGMuyWExQF-u-CVrxt7MLN-wpWNmD';
+const URL = `https://script.google.com/macros/s/${id}/exec`;
+const form = document.querySelector("#form-sheet");
+
+form.onsubmit = (e) => {
+	e.preventDefault();
+	const formData = new FormData(form);
+	const json = JSON.stringify(Object.fromEntries(formData));
+
+	fetch(URL, {
+		method: "post",
+		headers: {
+			"Content-Type": "text/plain;charset=utf-8",
+		},
+		body: json
+	})
+		.then((res) => res.json())
+		.then((res) => document.body.append(res.result))
+		.catch(err => document.body.append(err.message));
+};
